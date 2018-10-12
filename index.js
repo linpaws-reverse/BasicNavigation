dc53f81bf97cb7dcfb1fd7d61dc2337141b8ee31
Swapnil <swapnil@gatha.co.in>
2018-10-12 15:16:27 +0530

    2new feature of react native navigation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /** @format */
-
+//This is new feature supported by RN
+//SHow this
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
